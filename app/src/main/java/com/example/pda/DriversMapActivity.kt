ae8e4806ff15900a3020001e04ccb8a832ca3449
SydNs <sydneynsiimbe@gmail.com>
2019-02-14 14:53:43 +0300

    Added a fuctionality to record the users location to a firebase database reference which will later be used to draw a trail of his/her movement on the others maps

--- app/src/main/java/com/example/pda/DriversMapActivity.kt
+++ app/src/main/java/com/example/pda/DriversMapActivity.kt
@@ -12,14 +12,18 @@ import android.graphics.Canvas
 import android.graphics.drawable.Drawable
 import android.location.Location
 import android.os.Bundle
+import android.util.Log
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.RelativeLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.firebase.geofire.GeoFire
+import com.firebase.geofire.GeoLocation
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.location.LocationListener
@@ -31,7 +35,9 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.FirebaseDatabase.getInstance
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
@@ -58,19 +64,33 @@ open class DriversMapActivity : AppCompatActivity(), OnMapReadyCallback,
     private var txtName: TextView? = null
     private var txtPhone: TextView? = null
     private var settingsbtn: Button? = null
+    private var currentLogOutDriverStatus = false
+
 
     //    private var logoutbtn: Button? = null
 //    private var profilePic: CircleImageView? = null
     private var relativeLayout: RelativeLayout? = null
+    private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drivers_map)
+
+//        window.setFlags(
+//            WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+//            WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+//        )
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
 
-        logoutbtndriver
+        logoutbtndriver.setOnClickListener {
+            currentLogOutDriverStatus = true
+            DisconnectDriver()
+            mAuth?.signOut()
+            LogOutUser()
+        }
         settingsbtndriver
     }
 
@@ -97,8 +117,8 @@ open class DriversMapActivity : AppCompatActivity(), OnMapReadyCallback,
 
     override fun onConnected(p0: Bundle?) {
         locationRequest = LocationRequest()
-        locationRequest.interval = 10000
-        locationRequest.fastestInterval = 10000
+        locationRequest.interval = 100000
+        locationRequest.fastestInterval = 100000
         locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
 
         if (ActivityCompat.checkSelfPermission(
@@ -130,8 +150,7 @@ open class DriversMapActivity : AppCompatActivity(), OnMapReadyCallback,
 
         lastLocation = location
 
-        var icon = BitmapDescriptorFactory.fromResource(R.drawable.pda_driver)
-        var latlong = LatLng(location.latitude, location.longitude)
+        val latlong = LatLng(location.latitude, location.longitude)
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latlong))
         mMap.addMarker(
             MarkerOptions().position(latlong)
@@ -139,6 +158,32 @@ open class DriversMapActivity : AppCompatActivity(), OnMapReadyCallback,
                 )
         mMap.animateCamera(CameraUpdateFactory.zoomTo(17F))
         DisconnectDriver()
+
+        val userID: String? = FirebaseAuth.getInstance().currentUser?.uid
+        val DriversAvailabilityRef: DatabaseReference =
+            getInstance().reference.child("Drivers Available")
+        val geoFireAvailability = GeoFire(DriversAvailabilityRef)
+        val DriversWorkingRef: DatabaseReference =
+            getInstance().reference.child("Drivers Working")
+        val geoFireWorking = GeoFire(DriversWorkingRef)
+
+        when (customerID) {
+            "" -> {
+                geoFireWorking.removeLocation(userID)
+                geoFireAvailability.setLocation(
+                    userID,
+                    GeoLocation(location.latitude, location.longitude)
+                )
+            }
+            else -> {
+                geoFireAvailability.removeLocation(userID)
+                geoFireWorking.setLocation(
+                    userID,
+                    GeoLocation(location.latitude, location.longitude)
+                )
+            }
+        }
+
 
 
 
@@ -155,47 +200,32 @@ open class DriversMapActivity : AppCompatActivity(), OnMapReadyCallback,
     }
 
 
-    protected override fun onStop() {
+    override fun onStop() {
         super.onStop()
-        val userID: String? = FirebaseAuth.getInstance().currentUser?.uid
-        val database = getInstance()
-        val DriversAvailabiltyRef: DatabaseReference = database.reference.child("Drivers Available")
-        val geoFire = GeoFire(DriversAvailabiltyRef)
-        geoFire.removeLocation(userID)
+
+        if(!currentLogOutDriverStatus){
+            DisconnectDriver()
+        }
+
     }
 
     private fun DisconnectDriver() {
         val userID: String? = FirebaseAuth.getInstance().currentUser?.uid
         val database = Firebase.database.reference
 //        Toast.makeText(this,userID.toString(),Toast.LENGTH_SHORT).show()
-
         val DriversAvailabiltyRef: DatabaseReference = database.child("Drivers Available")
-        val geoFire = GeoFire(DriversAvailabiltyRef)
+        val geoFire= GeoFire(DriversAvailabiltyRef)
         geoFire.removeLocation(userID)
-//        when (customerID) {
-//            "" -> {
-//                geoFireWorking.removeLocation(userID)
-//                geoFireAvailability.setLocation(
-//                    userID,
-//                    GeoLocation(location.latitude, location.longitude)
-//                )
-//            }
-//            else -> {
-//                geoFireAvailability.removeLocation(userID)
-//                geoFireWorking.setLocation(
-//                    userID,
-//                    GeoLocation(location.latitude, location.longitude)
-//                )
-//            }
-//
-//
-//        }
+
     }
 
-    fun LogOutUser() {
-        val startPageIntent = Intent(this, WelcomeActivity::class.java)
-        startPageIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-        startActivity(startPageIntent)
+    private fun LogOutUser() {
+
+        Toast.makeText(this, "SuccessFully Logged Out", Toast.LENGTH_LONG).show()
+//        val startPageIntent = Intent(this, WelcomeActivity::class.java)
+//        startPageIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+//        startActivity(startPageIntent)
+        onBackPressed()
         finish()
     }
 
@@ -223,6 +253,25 @@ open class DriversMapActivity : AppCompatActivity(), OnMapReadyCallback,
         background.draw(canvas)
         vectorDrawable?.draw(canvas)
         return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        mAuth = FirebaseAuth.getInstance()
+        val currentUser: FirebaseUser? = mAuth?.currentUser
+
+        Log.e("error", currentUser.toString())
+        if (currentUser == null) {
+            startActivity(
+                Intent(
+                    this,
+                    WelcomeActivity::class.java
+                ).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            )
+            finish()
+        }
     }
 
 
